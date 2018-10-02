069ee7618d491ab1cf9567f677a920b68809b8eb
Camilo Carromeu <camilo@carromeu.com>
2018-10-02 11:14:43 -0400

    Changing code syntax to ES5, for fix error with UglifyJS build.

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-class WebP {
-    constructor() {
-        this.isSupportWebp = false;
 
-        try {
-            this.isSupportWebp = (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
-        } catch(e) {
-            console.warn('This browser not support Webp!');
-        }
-    }
+var WebP = module.exports = {}
 
-    install(Vue, config) {
-        Vue.prototype.$webp = this.isSupportWebp;
+WebP.isSupportWebp = false
 
-        Vue.directive('webp', (el, binding, vnode) => {
-            if (vnode.tag !== 'img') {
-                return
-            }
-            if (this.isSupportWebp) {
-                try {
-                    el.src = binding.value.toString().replace(/(\.jpg|\.png)/g, '.webp')
-                } catch(e) {
-                    console.error(e);
-                }
-
-                document.querySelector('html').classList.add('webp');
-            }
-        });
-    }
+try {
+    WebP.isSupportWebp = (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0)
+} catch(e) {
+    console.warn('This browser not support Webp!')
 }
 
-module.exports = new WebP();
+WebP.install = function (Vue, config) {
+    Vue.prototype.$webp = WebP.isSupportWebp;
+
+    Vue.directive('webp', function (el, binding, vnode) {
+        if (vnode.tag !== 'img') {
+            return
+        }
+        if (WebP.isSupportWebp) {
+            try {
+                el.src = binding.value.toString().replace(/(\.jpg|\.png)/g, '.webp')
+            } catch(e) {
+                console.error(e)
+            }
+
+            document.querySelector('html').classList.add('webp')
+        }
+    })
+}
